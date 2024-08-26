613aa978d4395f02db67e00a1513e6023b6af1b3
GizziXZ <bunclesgoodstuff@gmail.com>
2024-08-26 08:57:45 +0300

    encrypt command changes + Added decrypt command

---
+++ commands/utility/decrypt.js
@@ -0,0 +1,52 @@
+const { SlashCommandBuilder } = require('discord.js');
+const openpgp = require('openpgp');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('decrypt')
+        .setDescription('Decrypt a message using a private key')
+        .addStringOption(option => option.setName('passphrase').setDescription('Passphrase for the private key').setRequired(true))
+        .addStringOption(option => option.setName('encrypted').setDescription('The encrypted message to decrypt (without headers or footers)').setRequired(false))
+        .addStringOption(option => option.setName('privatekey').setDescription('Private key to decrypt with (armor without headers or footers)').setRequired(false))
+        .addAttachmentOption(option => option.setName('encryptedfile').setDescription('A file containing the encrypted message').setRequired(false))
+        .addAttachmentOption(option => option.setName('privatekeyfile').setDescription('A file containing the exported armor private key').setRequired(false)),
+    async execute(interaction) {
+        await interaction.deferReply();
+
+        let encryptedMessage = interaction.options.getString('encrypted');
+        let privateKeyArmored = interaction.options.getString('privatekey');
+        const encryptedFile = interaction.options.getAttachment('encryptedfile');
+        const privateKeyFile = interaction.options.getAttachment('privatekeyfile');
+
+        if (encryptedFile) {
+            const response = await fetch(encryptedFile.url);
+            encryptedMessage = await response.text();
+            // console.log(encryptedMessage);
+        } else if (!encryptedFile) {
+            encryptedMessage = `-----BEGIN PGP MESSAGE-----\n\n${encryptedMessage}\n-----END PGP MESSAGE-----`;
+        }
+
+        if (privateKeyFile) {
+            const response = await fetch(privateKeyFile.url);
+            privateKeyArmored = await response.text();
+            // console.log(privateKeyArmored);
+        } else if (!privateKeyFile) {
+            privateKeyArmored = `-----BEGIN PGP PRIVATE KEY BLOCK-----\n\n${privateKeyArmored}\n-----END PGP PRIVATE KEY BLOCK-----`;
+        }
+
+        if (!encryptedMessage || !privateKeyArmored) {
+            return interaction.editReply({ content: 'Both encrypted message and private key must be provided either as strings or attachments.' });
+        }
+
+        const privatekey = await openpgp.decryptKey({
+            privateKey: await openpgp.readPrivateKey({ armoredKey: privateKeyArmored }),
+            passphrase: interaction.options.getString('passphrase')
+        });
+        const decrypted = await openpgp.decrypt({
+            message: await openpgp.readMessage({ armoredMessage: encryptedMessage }),
+            decryptionKeys: privatekey,
+        });
+        // console.log(decrypted);
+        interaction.editReply({ content: decrypted.data });
+    }
+}
--- commands/utility/encrypt.js
+++ commands/utility/encrypt.js
@@ -6,11 +6,32 @@ module.exports = {
         .setName('encrypt')
         .setDescription('Encrypt a message using a public key')
         .addStringOption(option => option.setName('message').setDescription('The message to encrypt').setRequired(true))
-        .addStringOption(option => option.setName('publickey').setDescription('The public key to use').setRequired(true)),
+        .addStringOption(option => option.setName('publickey').setDescription('Public key to encrypt with (armor with no headers or footers)').setRequired(false))
+        .addAttachmentOption(option => option.setName('publickeyfile').setDescription('A file containing the exported armor public key').setRequired(false)),
     async execute(interaction) {
+        await interaction.deferReply();
+        
         const message = interaction.options.getString('message');
-        const publickey = await openpgp.readKey({ armoredKey: `-----BEGIN PGP PUBLIC KEY BLOCK-----\n\n${interaction.options.getString('publickey')}\n-----END PGP PRIVATE KEY BLOCK-----`});
+        const publicKeyFile = interaction.options.getAttachment('publickeyfile');
+        let publicKeyArmored = interaction.options.getString('publickey');
+        
+        if (publicKeyFile) {
+            const response = await fetch(publicKeyFile.url);
+            publicKeyArmored = await response.text();
+        } else if (!publicKeyFile) {
+            publicKeyArmored = `-----BEGIN PGP PUBLIC KEY BLOCK-----\n\n${publicKeyArmored}\n-----END PGP PUBLIC KEY BLOCK-----`;
+        }
 
-        console.log(publickey);
+        if (!publicKeyArmored) {
+            return interaction.editReply({ content: 'A public key must be provided either as a string or an attachment.' });
+        }
+
+        const encrypted = await openpgp.encrypt({
+            message: await openpgp.createMessage({ text: message }),
+            encryptionKeys: publicKeyArmored
+            // can add an optional signing key here
+        });
+        // console.log(encrypted);
+        interaction.editReply({ content: `\`\`\`${encrypted}\`\`\`` });
     }
 }
